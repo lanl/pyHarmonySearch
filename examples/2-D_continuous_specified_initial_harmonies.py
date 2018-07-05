62d0a1dafb2d1364514f10893e8ffd330b672016
Shing Hei Zhan <szhan@Shings-MacBook-Pro.local>
2018-07-04 20:12:01 -0700

    Initial harmonies were not actually passed

--- examples/2-D_continuous_specified_initial_harmonies.py
+++ examples/2-D_continuous_specified_initial_harmonies.py
@@ -120,5 +120,5 @@ if __name__ == '__main__':
     num_processes = 1
     num_iterations = 1  # because random_seed is defined, there's no point in running this multiple times
     initial_harmonies = [[123, -123] for _ in range(100)]    # set the initial harmonies for 100 individuals to [123, -123]
-    results = harmony_search(obj_fun, num_processes, num_iterations)
+    results = harmony_search(obj_fun, num_processes, num_iterations, initial_harmonies=initial_harmonies)
     print('Elapsed time: {}\nBest harmony: {}\nBest fitness: {}'.format(results.elapsed_time, results.best_harmony, results.best_fitness))
